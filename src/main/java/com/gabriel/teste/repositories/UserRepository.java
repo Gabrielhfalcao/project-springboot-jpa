b3df7ce4b7e2c0305406a87612ff8ab0f727c896
Gabriel.Falcao <gabrielfalchonorato18@gmail.com>
2024-01-15 20:24:23 -0300

    Service layer, component registration

--- src/main/java/com/gabriel/teste/repositories/UserRepository.java
+++ src/main/java/com/gabriel/teste/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.gabriel.teste.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gabriel.teste.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
