9196bbe06a07dba67069661266298ff0f0595de0
Gabriel.Falcao <gabrielfalchonorato18@gmail.com>
2024-01-16 00:34:58 -0300

    Category

--- src/main/java/com/gabriel/teste/config/TesteConfig.java
+++ src/main/java/com/gabriel/teste/config/TesteConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gabriel.teste.entities.Category;
 import com.gabriel.teste.entities.Order;
 import com.gabriel.teste.entities.User;
 import com.gabriel.teste.entities.enums.OrderStatus;
+import com.gabriel.teste.repositories.CategoryRepository;
 import com.gabriel.teste.repositories.OrderRepository;
 import com.gabriel.teste.repositories.UserRepository;
 
@@ -23,10 +25,19 @@ public class TesteConfig implements CommandLineRunner{
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
 
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456", null);
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456", null);
 
---
+++ src/main/java/com/gabriel/teste/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.gabriel.teste.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.teste.entities.Category;
+import com.gabriel.teste.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+		return category.get();
+	}
+	
+}
