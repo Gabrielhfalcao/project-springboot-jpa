3756437b51beed0f514942db59c6b426b906d470
Gabriel.Falcao <gabrielfalchonorato18@gmail.com>
2024-01-24 22:10:17 -0300

    Subtotal & Total methods

--- src/main/java/com/gabriel/teste/entities/Order.java
+++ src/main/java/com/gabriel/teste/entities/Order.java
@@ -86,6 +86,14 @@ public class Order implements Serializable {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		Double soma = 0.0;
+		for (OrderItem x : items) {
+			soma += x.getSubTotal();
+		}
+		return soma;
+	}
 
 	@Override
 	public int hashCode() {
