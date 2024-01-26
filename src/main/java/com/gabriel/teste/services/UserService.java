18e26defff1ebc19969f131e1012619df0f5e0f6
Gabriel Falcao <gabriel.honorato@kaspper.com.br>
2024-01-25 23:48:09 -0300

    Exception Handling - delete

--- src/main/java/com/gabriel/teste/services/UserService.java
+++ src/main/java/com/gabriel/teste/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.teste.entities.User;
 import com.gabriel.teste.repositories.UserRepository;
+import com.gabriel.teste.services.exceptions.DatabaseException;
 import com.gabriel.teste.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +32,14 @@ public class UserService {
 	}
 	
 	public void deleteById(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage())
+		}
+		
 	}
 	
 	public User updateById(Long id, User obj) {
